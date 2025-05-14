2425a616e1f64312ea6afee2340fb7ba41b20d62
Faiz Elfahad Kurniawan <74404911+siunlucky@users.noreply.github.com>
2025-05-14 20:13:04 +0700

    test

--- autiedu/answer.sql
+++ autiedu/answer.sql
@@ -3,6 +3,6 @@ create table autiedu.answer
     id          binary(16) default (uuid_to_bin(uuid())) not null
         primary key,
     question_id        binary(16)                            not null,
-    option_id binary(16)                                     not null,
+    option_id binary(16)                                     not null
 );
 
